ebe5395555060de7bed6e07f1e4c142a7132cc68
wadexhong <wadexhong@gmail.com>
2020-05-19 00:05:12 +0800

    fix: 修正 dialog add custom view 異常

--- app/src/main/java/com/bardxhong/activitytaskmaze/IntentFlagSelectView.kt
+++ app/src/main/java/com/bardxhong/activitytaskmaze/IntentFlagSelectView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatCheckBox
 
 class IntentFlagSelectView constructor(
     context: Context,
@@ -13,4 +14,11 @@ class IntentFlagSelectView constructor(
     init {
         View.inflate(context, R.layout.intent_flag_select_view, this)
     }
+
+    val cbNewTask: AppCompatCheckBox by lazy { findViewById<AppCompatCheckBox>(R.id.checkbox_new_task) }
+    val cbClearTop: AppCompatCheckBox by lazy { findViewById<AppCompatCheckBox>(R.id.checkbox_clear_top) }
+    val cbSingleTop: AppCompatCheckBox by lazy { findViewById<AppCompatCheckBox>(R.id.checkbox_single_top) }
+    val cbClearTask: AppCompatCheckBox by lazy { findViewById<AppCompatCheckBox>(R.id.checkbox_clear_task) }
+    val cbReorderToFront: AppCompatCheckBox by lazy { findViewById<AppCompatCheckBox>(R.id.checkbox_reorder_to_front) }
+    val cbIsCallFinish: AppCompatCheckBox by lazy { findViewById<AppCompatCheckBox>(R.id.checkbox_finish_after_intent) }
 }
--- app/src/main/java/com/bardxhong/activitytaskmaze/StartIntentButton.kt
+++ app/src/main/java/com/bardxhong/activitytaskmaze/StartIntentButton.kt
@@ -14,29 +14,22 @@ class StartIntentButton @JvmOverloads constructor(
     defStyle: Int = 0
 ) : androidx.appcompat.widget.AppCompatButton(context, attr, defStyle) {
 
-    private val selectView by lazy { IntentFlagSelectView(context) }
-    val cbNewTask: AppCompatCheckBox by lazy { selectView.findViewById<AppCompatCheckBox>(R.id.checkbox_new_task) }
-    val cbClearTop: AppCompatCheckBox by lazy { selectView.findViewById<AppCompatCheckBox>(R.id.checkbox_clear_top) }
-    val cbSingleTop: AppCompatCheckBox by lazy { selectView.findViewById<AppCompatCheckBox>(R.id.checkbox_single_top) }
-    val cbClearTask: AppCompatCheckBox by lazy { selectView.findViewById<AppCompatCheckBox>(R.id.checkbox_clear_task) }
-    val cbReorderToFront: AppCompatCheckBox by lazy { selectView.findViewById<AppCompatCheckBox>(R.id.checkbox_reorder_to_front) }
-    val cbIsCallFinish: AppCompatCheckBox by lazy { selectView.findViewById<AppCompatCheckBox>(R.id.checkbox_finish_after_intent) }
-
 
     fun showChoosingDialog(intent: Intent) {
+        val selectView = IntentFlagSelectView(context)
+
         AlertDialog.Builder(context)
             .setView(selectView)
             .setPositiveButton("send", object :DialogInterface.OnClickListener {
                 override fun onClick(dialog: DialogInterface?, which: Int) {
-                    if (cbNewTask.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    if (cbClearTop.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                    if (cbSingleTop.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-                    if (cbClearTask.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    if (cbReorderToFront.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
-                    context.startActivity(intent).also { if (cbIsCallFinish.isChecked) (context as? Activity)?.finish()}
+                    if (selectView.cbNewTask.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    if (selectView.cbClearTop.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    if (selectView.cbSingleTop.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                    if (selectView.cbClearTask.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    if (selectView.cbReorderToFront.isChecked) intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                    context.startActivity(intent).also { if (selectView.cbIsCallFinish.isChecked) (context as? Activity)?.finish()}
                 }
             })
             .show()
-        Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
     }
 }
